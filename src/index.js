d7f5fcf67758b771a3c994d2a521531b58e78bf7
Dustin Specker <dustinspecker@dustinspecker.com>
2015-11-12 17:17:33 -0600

    fix(src): use module.exports instead of default
    Babel 6 changed how default export works.

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@
  * @throws {Error} - if value is not a String
  * @return {Boolean} - is string a valid CSS color hex?
  */
-export default function isCSSColorHex(value) {
+module.exports = function (value) {
   if (typeof value !== 'string') {
     throw new Error('Expected value to be a String');
   }
 
   return (/^#[0-9a-fA-F]{3}([0-9a-fA-F]{3})?$/).test(value);
-}
+};
