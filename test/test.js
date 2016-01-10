13bfc6c58207fb02b4486632195589d1f97a1875
Dustin Specker <dustinspecker@dustinspecker.com>
2016-01-10 17:35:21 -0600

    chore(test): use AVA instead of mocha/chai

--- test/test.js
+++ test/test.js
@@ -1,80 +1,71 @@
-/* global describe, it */
 'use strict'
 import isCSSColorHex from '../lib/'
-import {expect} from 'chai'
+import test from 'ava'
 
-describe('is-css-color-hex', () => {
-  describe('error handling', () => {
-    it('should throw error if value is not passed', () => {
-      const test = () => isCSSColorHex()
-      expect(test).to.throw(Error, /Expected value to be a String/)
-    })
+test('should throw error if value is not passed', t => {
+  const shouldThrow = () => isCSSColorHex()
+  t.throws(shouldThrow, /Expected value to be a String/)
+})
 
-    it('should throw error if value is undefined', () => {
-      const test = () => isCSSColorHex(undefined)
-      expect(test).to.throw(Error, /Expected value to be a String/)
-    })
+test('should throw error if value is undefined', t => {
+  const shouldThrow = () => isCSSColorHex(undefined)
+  t.throws(shouldThrow, /Expected value to be a String/)
+})
 
-    it('should throw error if value is null', () => {
-      const test = () => isCSSColorHex(null)
-      expect(test).to.throw(Error, /Expected value to be a String/)
-    })
+test('should throw error if value is null', t => {
+  const shouldThrow = () => isCSSColorHex(null)
+  t.throws(shouldThrow, /Expected value to be a String/)
+})
 
-    it('should throw error if value is an array', () => {
-      const test = () => isCSSColorHex([])
-      expect(test).to.throw(Error, /Expected value to be a String/)
-    })
+test('should throw error if value is an array', t => {
+  const shouldThrow = () => isCSSColorHex([])
+  t.throws(shouldThrow, /Expected value to be a String/)
+})
 
-    it('should throw error if value is an object', () => {
-      const test = () => isCSSColorHex({})
-      expect(test).to.throw(Error, /Expected value to be a String/)
-    })
+test('should throw error if value is an object', t => {
+  const shouldThrow = () => isCSSColorHex({})
+  t.throws(shouldThrow, /Expected value to be a String/)
+})
 
-    it('should throw error if value is a number', () => {
-      const test = () => isCSSColorHex(1)
-      expect(test).to.throw(Error, /Expected value to be a String/)
-    })
-  })
+test('should throw error if value is a number', t => {
+  const shouldThrow = () => isCSSColorHex(1)
+  t.throws(shouldThrow, /Expected value to be a String/)
+})
 
-  describe('invalid CSS color hexes', () => {
-    it('should return false for string not prefixed with hash', () => {
-      expect(isCSSColorHex('000')).to.eql(false)
-      expect(isCSSColorHex('AAAAAA')).to.eql(false)
-      expect(isCSSColorHex('aaaaaa')).to.eql(false)
-    })
+test('should return false for string not prefixed with hash', t => {
+  t.notOk(isCSSColorHex('000'))
+  t.notOk(isCSSColorHex('AAAAAA'))
+  t.notOk(isCSSColorHex('aaaaaa'))
+})
 
-    it('should return false for hexes with other than 3 or 6 hex characters', () => {
-      expect(isCSSColorHex('#1')).to.eql(false)
-      expect(isCSSColorHex('#a')).to.eql(false)
-      expect(isCSSColorHex('#A')).to.eql(false)
-      expect(isCSSColorHex('#12')).to.eql(false)
-      expect(isCSSColorHex('#Ab')).to.eql(false)
-      expect(isCSSColorHex('#1234')).to.eql(false)
-      expect(isCSSColorHex('#AbCd')).to.eql(false)
-      expect(isCSSColorHex('#12345')).to.eql(false)
-      expect(isCSSColorHex('#AbCdE')).to.eql(false)
-      expect(isCSSColorHex('#1234567')).to.eql(false)
-      expect(isCSSColorHex('#aBcDeFf')).to.eql(false)
-    })
+test('should return false for hexes with other than 3 or 6 hex characters', t => {
+  t.notOk(isCSSColorHex('#1'))
+  t.notOk(isCSSColorHex('#a'))
+  t.notOk(isCSSColorHex('#A'))
+  t.notOk(isCSSColorHex('#12'))
+  t.notOk(isCSSColorHex('#Ab'))
+  t.notOk(isCSSColorHex('#1234'))
+  t.notOk(isCSSColorHex('#AbCd'))
+  t.notOk(isCSSColorHex('#12345'))
+  t.notOk(isCSSColorHex('#AbCdE'))
+  t.notOk(isCSSColorHex('#1234567'))
+  t.notOk(isCSSColorHex('#aBcDeFf'))
+})
 
-    it('should return false for invalid hex characters', () => {
-      expect(isCSSColorHex('#XYZ')).to.eql(false)
-      expect(isCSSColorHex('#xyz')).to.eql(false)
-      expect(isCSSColorHex('#XYZXYZ')).to.eql(false)
-      expect(isCSSColorHex('#xyz')).to.eql(false)
-      expect(isCSSColorHex('#!!!???')).to.eql(false)
-    })
-  })
+test('should return false for invalid hex characters', t => {
+  t.notOk(isCSSColorHex('#XYZ'))
+  t.notOk(isCSSColorHex('#xyz'))
+  t.notOk(isCSSColorHex('#XYZXYZ'))
+  t.notOk(isCSSColorHex('#xyz'))
+  t.notOk(isCSSColorHex('#!!!???'))
+})
 
-  describe('valid CSS color hexes', () => {
-    it('should true for valid color hexes', () => {
-      expect(isCSSColorHex('#000')).to.eql(true)
-      expect(isCSSColorHex('#AAA')).to.eql(true)
-      expect(isCSSColorHex('#aaa')).to.eql(true)
-      expect(isCSSColorHex('#000000')).to.eql(true)
-      expect(isCSSColorHex('#FF99CC')).to.eql(true)
-      expect(isCSSColorHex('#ff99cc')).to.eql(true)
-      expect(isCSSColorHex('#ff99CC')).to.eql(true)
-    })
-  })
+test('should true for valid color hexes', t => {
+  t.ok(isCSSColorHex('#000'))
+  t.ok(isCSSColorHex('#AAA'))
+  t.ok(isCSSColorHex('#aaa'))
+  t.ok(isCSSColorHex('#000000'))
+  t.ok(isCSSColorHex('#FF99CC'))
+  t.ok(isCSSColorHex('#ff99cc'))
+  t.ok(isCSSColorHex('#ff99CC'))
 })
